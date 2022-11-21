43a32c749e6957f9337e6db6a2e2b7a289c9b24e
gongxini <yigongh@gmail.com>
2022-11-21 00:40:38 -0500

    update the script

--- sosp_aec/psandbox_script/mysql_server5.sh
+++ sosp_aec/psandbox_script/mysql_server5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysqld --defaults-file=./mysql.cnf &
+mysqld --defaults-file=${PSP_DIR}/sosp_aec/psandbox_script/mysql.cnf &
 sysbench --mysql-socket=$PSANDBOX_MYSQL_DIR/mysqld.sock --mysql-db=test --tables=1 --table-size=1 --threads=1 --time=65 $SYSBEN_DIR/oltp_update_index.lua --report-interval=3 cleanup >> /dev/null
 sysbench --mysql-socket=$PSANDBOX_MYSQL_DIR/mysqld.sock --mysql-db=test --tables=1 --table-size=1 --threads=1 --time=65 $SYSBEN_DIR/oltp_update_index.lua --report-interval=3 prepare >> /dev/null
 
